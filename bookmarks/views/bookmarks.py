0177660cf6b564aeef6ea83bac628ca6e1aeec61
Sascha Ißbrücker <sascha.issbruecker@byteagenten.de>
2019-06-29 09:15:07 +0200

    Implement basic paging

--- bookmarks/views/bookmarks.py
+++ bookmarks/views/bookmarks.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from django.http import HttpResponseRedirect, HttpRequest
 from django.shortcuts import render
 from django.urls import reverse
@@ -5,12 +6,22 @@ from django.urls import reverse
 from bookmarks.models import Bookmark, BookmarkForm
 from bookmarks.services.bookmarks import create_bookmark, update_bookmark
 
+_default_page_size = 30
+
 
 def index(request):
+    page = request.GET.get('page')
+    paginator = Paginator(_get_bookmark_list(), _default_page_size)
+    bookmarks = paginator.get_page(page)
+
     context = {
-        'bookmarks': Bookmark.objects.all()
+        'bookmarks': bookmarks
     }
     return render(request, 'bookmarks/index.html', context)
+
+
+def _get_bookmark_list():
+    return Bookmark.objects.order_by('-date_added')
 
 
 def new(request: HttpRequest):
