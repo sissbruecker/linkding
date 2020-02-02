ef1c48508273150c177b2641b6c1c61eec306a45
Daniele Bonomi <github@notmycloud.com>
2020-02-02 16:27:18 +0100

    Drop privileges

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,10 +3,14 @@
 
 # Create data folder if it does not exist
 mkdir -p data
+
 # Run database migration
 python manage.py migrate
 # Generate secret key file if it does not exist
 python manage.py generate_secret_key
+
+# Ensure the DB folder is owned by the right user
+chown -R www-data: /etc/linkding/data
 
 # Start uwsgi server
 uwsgi uwsgi.ini
