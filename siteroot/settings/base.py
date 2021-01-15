94eb55896d0994a02577b2475a60d9366b165e96
Sascha Ißbrücker <sissbruecker@lyska.io>
2021-01-16 00:29:37 +0100

    Fix default API permissions

--- siteroot/settings/base.py
+++ siteroot/settings/base.py
@@ -151,6 +151,9 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',
     ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated'
+    ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 100
 }
