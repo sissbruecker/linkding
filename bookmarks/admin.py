13b638e60df0c69201701d7257110f6b8cdd574b
Sascha Ißbrücker <sissbruecker@lyska.io>
2021-02-16 17:24:05 +0100

    Improve and promote admin panel (#76)

--- bookmarks/admin.py
+++ bookmarks/admin.py
@@ -1,17 +1,69 @@
-from django.contrib import admin
+from django.contrib import admin, messages
+from django.db.models import Count, QuerySet
+from django.utils.translation import ngettext, gettext
 
 from bookmarks.models import Bookmark, Tag
+from bookmarks.services.bookmarks import archive_bookmark, unarchive_bookmark
+
 
 @admin.register(Bookmark)
 class AdminBookmark(admin.ModelAdmin):
-  list_display = ('title', 'url', 'date_added')
-  search_fields = ('title', 'url', 'tags__name')
-  list_filter = ('tags',)
-  ordering = ('-date_added', )
+    list_display = ('resolved_title', 'url', 'owner', 'date_added', 'is_archived')
+    search_fields = ('title', 'description', 'website_title', 'website_description', 'url', 'tags__name')
+    list_filter = ('owner__username', 'is_archived', 'tags',)
+    ordering = ('-date_added',)
+    actions = ['archive_selected_bookmarks', 'unarchive_selected_bookmarks']
+
+    def archive_selected_bookmarks(self, request, queryset: QuerySet):
+        for bookmark in queryset:
+            archive_bookmark(bookmark)
+        bookmarks_count = queryset.count()
+        self.message_user(request, ngettext(
+            '%d bookmark was successfully archived.',
+            '%d bookmarks were successfully archived.',
+            bookmarks_count,
+        ) % bookmarks_count, messages.SUCCESS)
+
+    def unarchive_selected_bookmarks(self, request, queryset: QuerySet):
+        for bookmark in queryset:
+            unarchive_bookmark(bookmark)
+        bookmarks_count = queryset.count()
+        self.message_user(request, ngettext(
+            '%d bookmark was successfully unarchived.',
+            '%d bookmarks were successfully unarchived.',
+            bookmarks_count,
+        ) % bookmarks_count, messages.SUCCESS)
+
 
 @admin.register(Tag)
 class AdminTag(admin.ModelAdmin):
-  list_display = ('name', 'date_added', 'owner')
-  search_fields = ('name', 'owner__username')
-  list_filter = ('owner__username', )
-  ordering = ('-date_added', )
+    list_display = ('name', 'date_added', 'owner', 'bookmarks_count')
+    search_fields = ('name', 'owner__username')
+    list_filter = ('owner__username',)
+    ordering = ('-date_added',)
+    actions = ['delete_unused_tags']
+
+    def get_queryset(self, request):
+        queryset = super().get_queryset(request)
+        queryset = queryset.annotate(bookmarks_count=Count("bookmark"))
+        return queryset
+
+    def bookmarks_count(self, obj):
+        return obj.bookmarks_count
+
+    def delete_unused_tags(self, request, queryset: QuerySet):
+        unused_tags = queryset.filter(bookmark__isnull=True)
+        unused_tags_count = unused_tags.count()
+        for tag in unused_tags:
+            tag.delete()
+
+        if unused_tags_count > 0:
+            self.message_user(request, ngettext(
+                '%d unused tag was successfully deleted.',
+                '%d unused tags were successfully deleted.',
+                unused_tags_count,
+            ) % unused_tags_count, messages.SUCCESS)
+        else:
+            self.message_user(request, gettext(
+                'There were no unused tags in the selection',
+            ), messages.SUCCESS)
