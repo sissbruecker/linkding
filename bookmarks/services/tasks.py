183089cfd34abe081feeb75e83ec29290c55a58c
Sascha Ißbrücker <sascha.issbruecker@googlemail.com>
2021-08-17 12:20:29 +0200

    simplify bookmark null check

--- bookmarks/services/tasks.py
+++ bookmarks/services/tasks.py
@@ -11,25 +11,21 @@ logger = logging.getLogger(__name__)
 
 @background()
 def create_web_archive_snapshot(bookmark_id: int):
-    bookmark = None
     try:
         bookmark = Bookmark.objects.get(id=bookmark_id)
     except Bookmark.DoesNotExist:
-        pass
-
-    if not bookmark:
         return
 
-    logger.debug(f'Saving archive for bookmark: {bookmark}...')
+    logger.debug(f'Create web archive link for bookmark: {bookmark}...')
 
     wayback = waybackpy.Url(bookmark.url)
 
     try:
         archive = wayback.save()
     except WaybackError as error:
-        logger.exception(f'Error saving for bookmark: {bookmark}...', exc_info=error)
+        logger.exception(f'Error creating web archive link for bookmark: {bookmark}...', exc_info=error)
         raise
 
     bookmark.web_archive_snapshot_url = archive.archive_url
     bookmark.save()
-    logger.debug(f'Successfully saved archive for bookmark: {bookmark}...')
+    logger.debug(f'Successfully created web archive link for bookmark: {bookmark}...')
