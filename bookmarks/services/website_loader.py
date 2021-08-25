07fe478d9f9dfb1d74f61af4aecced3b4dc79c20
Sascha Ißbrücker <sascha.issbruecker@googlemail.com>
2021-08-25 10:07:42 +0200

    use charset_normalizer to determine response encoding

--- bookmarks/services/website_loader.py
+++ bookmarks/services/website_loader.py
@@ -2,6 +2,7 @@ from dataclasses import dataclass
 
 import requests
 from bs4 import BeautifulSoup
+from charset_normalizer import from_bytes
 
 
 @dataclass
@@ -34,4 +35,10 @@ def load_website_metadata(url: str):
 
 def load_page(url: str):
     r = requests.get(url, timeout=10)
-    return r.content
+
+    # Use charset_normalizer to determine encoding that best matches the response content
+    # Several sites seem to specify the response encoding incorrectly, so we ignore it and use custom logic instead
+    # This is different from Response.text which does respect the encoding specified in the response first,
+    # before trying to determine one
+    results = from_bytes(r.content)
+    return str(results.best())
