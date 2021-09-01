3396d5db5da7b34dfea3e3f60f62957fbb87e358
Christoph Schmatzler <christoph@medium.place>
2021-09-01 15:51:41 +0200

    Allow specifying port through LINKDING_PORT environment variable

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 # Bootstrap script that gets executed in new Docker containers
 
+LINKDING_PORT="${LINKDING_PORT:-9090}"
+
 # Create data folder if it does not exist
 mkdir -p data
 
@@ -13,4 +15,4 @@ python manage.py generate_secret_key
 chown -R www-data: /etc/linkding/data
 
 # Start uwsgi server
-uwsgi uwsgi.ini
+uwsgi --http :$LINKDING_PORT uwsgi.ini
