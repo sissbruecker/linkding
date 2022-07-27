a558199fbe31d1ec1218a2c6abbe961c6883a909
Sascha Ißbrücker <sascha.issbruecker@gmail.com>
2022-07-27 21:24:21 +0200

    Add tag cloud tests

--- bookmarks/tests/test_tag_cloud_tag.py
+++ bookmarks/tests/test_tag_cloud_tag.py
@@ -12,16 +12,17 @@ class TagCloudTagTest(TestCase, BookmarkFactoryMixin):
     def make_soup(self, html: str):
         return BeautifulSoup(html, features="html.parser")
 
-    def render_template(self, tags: List[Tag], url: str = '/test'):
+    def render_template(self, tags: List[Tag], selected_tags: List[Tag] = [], url: str = '/test'):
         rf = RequestFactory()
         request = rf.get(url)
         context = RequestContext(request, {
             'request': request,
             'tags': tags,
+            'selected_tags': selected_tags,
         })
         template_to_render = Template(
             '{% load bookmarks %}'
-            '{% tag_cloud tags %}'
+            '{% tag_cloud tags selected_tags %}'
         )
         return template_to_render.render(context)
 
@@ -40,6 +41,11 @@ class TagCloudTagTest(TestCase, BookmarkFactoryMixin):
             for tag_index, tag in enumerate(tags, start=0):
                 link_element = link_elements[tag_index]
                 self.assertEqual(link_element.text.strip(), tag)
+
+    def assertNumSelectedTags(self, rendered_template: str, count: int):
+        soup = self.make_soup(rendered_template)
+        link_elements = soup.select('p.selected-tags a')
+        self.assertEqual(len(link_elements), count)
 
     def test_group_alphabetically(self):
         tags = [
@@ -91,4 +97,54 @@ class TagCloudTagTest(TestCase, BookmarkFactoryMixin):
             [
                 'shared',
             ],
+        ])
+
+    def test_selected_tags(self):
+        tags = [
+            self.setup_tag(name='tag1'),
+            self.setup_tag(name='tag2'),
+            self.setup_tag(name='tag3'),
+            self.setup_tag(name='tag4'),
+            self.setup_tag(name='tag5'),
+        ]
+        selected_tags = [
+            tags[0],
+            tags[1],
+        ]
+
+        rendered_template = self.render_template(tags, selected_tags, url='/test?q=%23tag1 %23tag2 %23tag3')
+
+        self.assertNumSelectedTags(rendered_template, 2)
+
+        self.assertInHTML('''
+            <a href="?q=%23tag2+%23tag3"
+               class="text-bold mr-2">
+                <span>-tag1</span>
+            </a>
+        ''', rendered_template)
+
+        self.assertInHTML('''
+            <a href="?q=%23tag1+%23tag3"
+               class="text-bold mr-2">
+                <span>-tag2</span>
+            </a>
+        ''', rendered_template)
+
+    def test_selected_tags_are_excluded_from_groups(self):
+        tags = [
+            self.setup_tag(name='tag1'),
+            self.setup_tag(name='tag2'),
+            self.setup_tag(name='tag3'),
+            self.setup_tag(name='tag4'),
+            self.setup_tag(name='tag5'),
+        ]
+        selected_tags = [
+            tags[0],
+            tags[1],
+        ]
+
+        rendered_template = self.render_template(tags, selected_tags)
+
+        self.assertTagGroups(rendered_template, [
+            ['tag3', 'tag4', 'tag5']
         ])
