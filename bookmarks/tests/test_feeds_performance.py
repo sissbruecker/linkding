31902e24d7c994fa5d8d00496bafe427e0a28171
Sascha Ißbrücker <sascha.issbruecker@gmail.com>
2022-09-04 21:30:51 +0200

    add feeds performance test

---
+++ bookmarks/tests/test_feeds_performance.py
@@ -0,0 +1,35 @@
+from django.db import connections
+from django.db.utils import DEFAULT_DB_ALIAS
+from django.test import TestCase
+from django.test.utils import CaptureQueriesContext
+from django.urls import reverse
+
+from bookmarks.models import FeedToken
+from bookmarks.tests.helpers import BookmarkFactoryMixin
+
+
+class FeedsPerformanceTestCase(TestCase, BookmarkFactoryMixin):
+
+    def setUp(self) -> None:
+        user = self.get_or_create_test_user()
+        self.client.force_login(user)
+        self.token = FeedToken.objects.get_or_create(user=user)[0]
+
+    def get_connection(self):
+        return connections[DEFAULT_DB_ALIAS]
+
+    def test_all_max_queries(self):
+        # set up some bookmarks with associated tags
+        num_initial_bookmarks = 10
+        for index in range(num_initial_bookmarks):
+            self.setup_bookmark(tags=[self.setup_tag()])
+
+        # capture number of queries
+        context = CaptureQueriesContext(self.get_connection())
+        with context:
+            feed_url = reverse('bookmarks:feeds.all', args=[self.token.key])
+            self.client.get(feed_url)
+
+        number_of_queries = context.final_queries
+
+        self.assertLess(number_of_queries, num_initial_bookmarks)
