2efb5b61e10c53511364fcfb417e3c27f7affae1
McKenna Jones <mckenna.jones@mediakind.com>
2023-01-13 12:39:35 -0700

    add simple health endpoint

--- bookmarks/middlewares.py
+++ bookmarks/middlewares.py
@@ -1,6 +1,30 @@
 from django.conf import settings
 from django.contrib.auth.middleware import RemoteUserMiddleware
-
+from django.http import JsonResponse
+from django.db import connections
+from bookmarks.views.settings import app_version
 
 class CustomRemoteUserMiddleware(RemoteUserMiddleware):
     header = settings.LD_AUTH_PROXY_USERNAME_HEADER
+
+class HealthCheckMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        if request.path == '/health':
+            code = 200
+            response = {
+                'version': app_version,
+                'status': 'healthy'
+                }
+
+            try:
+                connections['default'].ensure_connection()
+            except Exception as e:
+                response['status'] = 'unhealthy'
+                code = 500
+
+            return JsonResponse(response, status=code)
+        
+        return self.get_response(request)
--- siteroot/settings/base.py
+++ siteroot/settings/base.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'bookmarks.middlewares.HealthCheckMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
