2fd66522b955882abbea974e6b72d32b9b535a66
Sascha Ißbrücker <sascha.issbruecker@gmail.com>
2023-01-19 21:13:58 +0100

    Cleanup

--- bookmarks/views/health.py
+++ bookmarks/views/health.py
@@ -1,19 +1,19 @@
-from django.conf import settings
-from django.contrib.auth.middleware import RemoteUserMiddleware
-from django.http import JsonResponse
 from django.db import connections
+from django.http import JsonResponse
+
 from bookmarks.views.settings import app_version
+
 
 def health(request):
     code = 200
     response = {
         'version': app_version,
         'status': 'healthy'
-        }
+    }
 
     try:
         connections['default'].ensure_connection()
-    except Exception as e:
+    except Exception:
         response['status'] = 'unhealthy'
         code = 500
 
