c1ae466e9cd402e99c36ed532e310b98b00105b5
Sascha Ißbrücker <sascha.issbruecker@gmail.com>
2023-01-20 21:30:48 +0100

    Revert changes to middlewares.py

--- bookmarks/middlewares.py
+++ bookmarks/middlewares.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.contrib.auth.middleware import RemoteUserMiddleware
 
+
 class CustomRemoteUserMiddleware(RemoteUserMiddleware):
     header = settings.LD_AUTH_PROXY_USERNAME_HEADER
