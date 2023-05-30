873d90130bbb82f4eade926158cc726527a26d52
Andrew Barrow <60839662+acbgbca@users.noreply.github.com>
2023-05-30 03:41:36 +0000

    Updated bookmarklet to pass site title #118

--- bookmarks/templates/bookmarks/bookmarklet.js
+++ bookmarks/templates/bookmarks/bookmarklet.js
@@ -1,8 +1,10 @@
 (function () {
   var bookmarkUrl = window.location;
+  var bookmarkTitle = document.title;
   var applicationUrl = '{{ application_url }}';
 
   applicationUrl += '?url=' + encodeURIComponent(bookmarkUrl);
+  applicationUrl += '&title=' + encodeURIComponent(bookmarkTitle);
   applicationUrl += '&auto_close';
 
   window.open(applicationUrl);
