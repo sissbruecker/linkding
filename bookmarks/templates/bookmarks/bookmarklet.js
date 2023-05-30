a495c7dd45da2488cc64851070f498f2571d8bc9
Andrew Barrow <60839662+acbgbca@users.noreply.github.com>
2023-05-30 06:44:35 +0000

    Revert "Updated bookmarklet to pass site title #118"
    This reverts commit 873d90130bbb82f4eade926158cc726527a26d52.

--- bookmarks/templates/bookmarks/bookmarklet.js
+++ bookmarks/templates/bookmarks/bookmarklet.js
@@ -1,10 +1,8 @@
 (function () {
   var bookmarkUrl = window.location;
-  var bookmarkTitle = document.title;
   var applicationUrl = '{{ application_url }}';
 
   applicationUrl += '?url=' + encodeURIComponent(bookmarkUrl);
-  applicationUrl += '&title=' + encodeURIComponent(bookmarkTitle);
   applicationUrl += '&auto_close';
 
   window.open(applicationUrl);
