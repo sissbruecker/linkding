4aaaf9f2bb80c398ea5d51acaa5a7f3d4534a482
Sascha Ißbrücker <sascha.issbruecker@gmail.com>
2023-08-20 19:57:35 +0200

    Refactor confirm button JS into web component

--- bookmarks/static/shared.js
+++ bookmarks/static/shared.js
@@ -1,92 +1,92 @@
+class ConfirmButton extends HTMLElement {
+  connectedCallback() {
+    const button = this.querySelector("button");
+    button.dataset.type = button.type;
+    button.dataset.name = button.name;
+    button.dataset.value = button.value;
+    button.removeAttribute("type");
+    button.removeAttribute("name");
+    button.removeAttribute("value");
+    button.addEventListener("click", this.onClick.bind(this));
+    this.button = button;
+  }
+
+  onClick(event) {
+    event.preventDefault();
+
+    const cancelButton = document.createElement(this.button.nodeName);
+    cancelButton.type = "button";
+    cancelButton.innerText = "Cancel";
+    cancelButton.className = "btn btn-link btn-sm btn-confirmation-action mr-1";
+    cancelButton.addEventListener("click", this.reset.bind(this));
+
+    const confirmButton = document.createElement(this.button.nodeName);
+    confirmButton.type = this.button.dataset.type;
+    confirmButton.name = this.button.dataset.name;
+    confirmButton.value = this.button.dataset.value;
+    confirmButton.innerText = "Confirm";
+    confirmButton.className =
+      "btn btn-link btn-delete btn-sm btn-confirmation-action";
+    confirmButton.addEventListener("click", this.reset.bind(this));
+
+    const container = document.createElement("span");
+    container.className = "confirmation";
+    container.append(cancelButton, confirmButton);
+    this.container = container;
+
+    this.button.before(container);
+    this.button.classList.add("d-none");
+  }
+
+  reset() {
+    setTimeout(() => {
+      this.container.remove();
+      this.button.classList.remove("d-none");
+    });
+  }
+}
+
+customElements.define("ld-confirm-button", ConfirmButton);
+
 (function () {
+  function initGlobalShortcuts() {
+    // Focus search button
+    document.addEventListener("keydown", function (event) {
+      // Filter for shortcut key
+      if (event.key !== "s") return;
+      // Skip if event occurred within an input element
+      const targetNodeName = event.target.nodeName;
+      const isInputTarget =
+        targetNodeName === "INPUT" ||
+        targetNodeName === "SELECT" ||
+        targetNodeName === "TEXTAREA";
 
-    function initConfirmationButtons() {
-        const buttonEls = document.querySelectorAll('.btn-confirmation');
+      if (isInputTarget) return;
 
-        function showConfirmation(buttonEl) {
-            const cancelEl = document.createElement(buttonEl.nodeName);
-            cancelEl.innerText = 'Cancel';
-            cancelEl.className = 'btn btn-link btn-sm btn-confirmation-action mr-1';
-            cancelEl.addEventListener('click', function () {
-                container.remove();
-                buttonEl.style = '';
-            });
+      const searchInput = document.querySelector('input[type="search"]');
 
-            const confirmEl = document.createElement(buttonEl.nodeName);
-            confirmEl.innerText = 'Confirm';
-            confirmEl.className = 'btn btn-link btn-delete btn-sm btn-confirmation-action';
-            confirmEl.addEventListener('click', function () {
-                setTimeout(() => {
-                    container.remove();
-                    buttonEl.style = '';
-                });
-            });
+      if (searchInput) {
+        searchInput.focus();
+        event.preventDefault();
+      }
+    });
 
-            if (buttonEl.nodeName === 'BUTTON') {
-                confirmEl.type = buttonEl.dataset.type;
-                confirmEl.name = buttonEl.dataset.name;
-                confirmEl.value = buttonEl.dataset.value;
-            }
+    // Add new bookmark
+    document.addEventListener("keydown", function (event) {
+      // Filter for new entry shortcut key
+      if (event.key !== "n") return;
+      // Skip if event occurred within an input element
+      const targetNodeName = event.target.nodeName;
+      const isInputTarget =
+        targetNodeName === "INPUT" ||
+        targetNodeName === "SELECT" ||
+        targetNodeName === "TEXTAREA";
 
-            const container = document.createElement('span');
-            container.className = 'confirmation'
-            container.appendChild(cancelEl);
-            container.appendChild(confirmEl);
-            buttonEl.parentElement.insertBefore(container, buttonEl);
-            buttonEl.style = 'display: none';
-        }
+      if (isInputTarget) return;
 
-        buttonEls.forEach(function (buttonEl) {
-            buttonEl.dataset.type = buttonEl.type;
-            buttonEl.dataset.name = buttonEl.name;
-            buttonEl.dataset.value = buttonEl.value;
-            buttonEl.removeAttribute('type');
-            buttonEl.removeAttribute('name');
-            buttonEl.removeAttribute('value');
-            buttonEl.addEventListener('click', function (e) {
-                e.preventDefault();
-                showConfirmation(buttonEl);
-            });
-        });
-    }
+      window.location.assign("/bookmarks/new");
+    });
+  }
 
-    function initGlobalShortcuts() {
-        // Focus search button
-        document.addEventListener('keydown', function (event) {
-            // Filter for shortcut key
-            if (event.key !== 's') return;
-            // Skip if event occurred within an input element
-            const targetNodeName = event.target.nodeName;
-            const isInputTarget = targetNodeName === 'INPUT'
-                || targetNodeName === 'SELECT'
-                || targetNodeName === 'TEXTAREA';
-
-            if (isInputTarget) return;
-
-            const searchInput = document.querySelector('input[type="search"]');
-
-            if (searchInput) {
-                searchInput.focus();
-                event.preventDefault();
-            }
-        });
-
-        // Add new bookmark
-        document.addEventListener('keydown', function(event) {
-            // Filter for new entry shortcut key
-            if (event.key !== 'n') return;
-            // Skip if event occurred within an input element
-            const targetNodeName = event.target.nodeName;
-            const isInputTarget = targetNodeName === 'INPUT'
-                || targetNodeName === 'SELECT'
-                || targetNodeName === 'TEXTAREA';
-
-            if (isInputTarget) return;
-
-            window.location.assign("/bookmarks/new");
-        });
-    }
-
-    initConfirmationButtons();
-    initGlobalShortcuts();
-})()
+  initGlobalShortcuts();
+})();
