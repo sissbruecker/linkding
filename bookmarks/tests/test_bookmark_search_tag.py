f69b82cb9cd3268d2467a22085861c087d0fc20d
Sascha Ißbrücker <sascha.issbruecker@gmail.com>
2023-09-01 23:10:31 +0200

    Add shared filter UI

--- bookmarks/tests/test_bookmark_search_tag.py
+++ bookmarks/tests/test_bookmark_search_tag.py
@@ -44,11 +44,13 @@ class BookmarkSearchTagTest(TestCase, BookmarkFactoryMixin):
         self.assertNoHiddenInput(rendered_template, 'user')
         self.assertNoHiddenInput(rendered_template, 'q')
         self.assertNoHiddenInput(rendered_template, 'sort')
+        self.assertNoHiddenInput(rendered_template, 'filter_shared')
 
         # With params
-        url = '/test?q=foo&user=john&sort=title_asc'
+        url = '/test?q=foo&user=john&sort=title_asc&filter_shared=yes'
         rendered_template = self.render_template(url)
 
         self.assertHiddenInput(rendered_template, 'user', 'john')
         self.assertNoHiddenInput(rendered_template, 'q')
         self.assertNoHiddenInput(rendered_template, 'sort')
+        self.assertNoHiddenInput(rendered_template, 'filter_shared')
