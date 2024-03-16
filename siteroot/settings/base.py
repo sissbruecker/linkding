aedba4eacddaf7e516f7d1e30ea3ca7d55c953c5
Sascha Ißbrücker <sascha.issbruecker@gmail.com>
2024-03-16 21:41:30 +0100

    add dependency, update settings

--- siteroot/settings/base.py
+++ siteroot/settings/base.py
@@ -183,6 +183,24 @@ MAX_ATTEMPTS = 5
 BACKGROUND_TASK_RUN_ASYNC = True
 BACKGROUND_TASK_ASYNC_THREADS = 2
 
+# Enable OICD support if configured
+LD_ENABLE_OIDC = os.getenv("LD_ENABLE_OIDC", False) in (True, "True", "1")
+
+AUTHENTICATION_BACKENDS = ["django.contrib.auth.backends.ModelBackend"]
+
+if LD_ENABLE_OIDC:
+    AUTHENTICATION_BACKENDS.append("mozilla_django_oidc.auth.OIDCAuthenticationBackend")
+
+    OIDC_USERNAME_ALGO = "bookmarks.utils.generate_username"
+    OIDC_RP_CLIENT_ID = os.getenv("OIDC_RP_CLIENT_ID")
+    OIDC_OP_AUTHORIZATION_ENDPOINT = os.getenv("OIDC_OP_AUTHORIZATION_ENDPOINT")
+    OIDC_OP_TOKEN_ENDPOINT = os.getenv("OIDC_OP_TOKEN_ENDPOINT")
+    OIDC_OP_USER_ENDPOINT = os.getenv("OIDC_OP_USER_ENDPOINT")
+    OIDC_USE_PKCE = os.getenv("OIDC_USE_PKCE", True)
+    OIDC_RP_CLIENT_SECRET = os.getenv("OIDC_RP_CLIENT_SECRET")
+    OIDC_RP_SIGN_ALGO = os.getenv("OIDC_RP_SIGN_ALGO", "HS256")
+    OIDC_OP_JWKS_ENDPOINT = os.getenv("OIDC_OP_JWKS_ENDPOINT")
+
 # Enable authentication proxy support if configured
 LD_ENABLE_AUTH_PROXY = os.getenv("LD_ENABLE_AUTH_PROXY", False) in (True, "True", "1")
 LD_AUTH_PROXY_USERNAME_HEADER = os.getenv(
@@ -190,29 +208,12 @@ LD_AUTH_PROXY_USERNAME_HEADER = os.getenv(
 )
 LD_AUTH_PROXY_LOGOUT_URL = os.getenv("LD_AUTH_PROXY_LOGOUT_URL", None)
 
-LD_ENABLE_OIDC = os.getenv('LD_ENABLE_OIDC', False) in (True, 'True', '1')
-
-AUTHENTICATION_BACKENDS = ["django.contrib.auth.backends.ModelBackend",]
-
-if LD_ENABLE_OIDC:
-    AUTHENTICATION_BACKENDS.append('mozilla_django_oidc.auth.OIDCAuthenticationBackend')
-    OIDC_USERNAME_ALGO = 'bookmarks.utils.generate_username'
-
-    OIDC_RP_SIGN_ALGO = os.getenv("OIDC_RP_SIGN_ALGO")
-    OIDC_OP_JWKS_ENDPOINT = os.getenv("OIDC_OP_JWKS_ENDPOINT")
-    OIDC_OP_AUTHORIZATION_ENDPOINT = os.getenv("OIDC_OP_AUTHORIZATION_ENDPOINT")
-    OIDC_OP_TOKEN_ENDPOINT = os.getenv("OIDC_OP_TOKEN_ENDPOINT")
-    OIDC_OP_USER_ENDPOINT = os.getenv("OIDC_OP_USER_ENDPOINT")
-
-    OIDC_RP_CLIENT_ID = os.getenv('OIDC_RP_CLIENT_ID')
-    OIDC_RP_CLIENT_SECRET = os.getenv('OIDC_RP_CLIENT_SECRET')
-
 if LD_ENABLE_AUTH_PROXY:
     # Add middleware that automatically authenticates requests that have a known username
     # in the LD_AUTH_PROXY_USERNAME_HEADER request header
     MIDDLEWARE.append("bookmarks.middlewares.CustomRemoteUserMiddleware")
     # Configure auth backend that does not require a password credential
-    AUTHENTICATION_BACKENDS.append("django.contrib.auth.backends.RemoteUserBackend")
+    AUTHENTICATION_BACKENDS = ["django.contrib.auth.backends.RemoteUserBackend"]
     # Configure logout URL
     if LD_AUTH_PROXY_LOGOUT_URL:
         LOGOUT_REDIRECT_URL = LD_AUTH_PROXY_LOGOUT_URL
