995e5983e8d6a3251baa9ad33a99251344a7c74a
Sascha Ißbrücker <sascha.issbruecker@gmail.com>
2024-03-16 22:51:17 +0100

    add tests

--- bookmarks/utils.py
+++ bookmarks/utils.py
@@ -120,4 +120,4 @@ def generate_username(email):
     # Using Python 3 and Django 1.11+, usernames can contain alphanumeric
     # (ascii and unicode), _, @, +, . and - characters. So we normalize
     # it and slice at 150 characters.
-    return unicodedata.normalize('NFKC', email)[:150]
+    return unicodedata.normalize("NFKC", email)[:150]
