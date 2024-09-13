3907d8412859f7c757bf12a4bb45cd89e0c1aa36
Sascha Ißbrücker <sascha.issbruecker@gmail.com>
2024-09-13 22:59:27 +0200

    update test scripts

--- scripts/test-e2e.sh
+++ scripts/test-e2e.sh
@@ -6,9 +6,7 @@ playwright install chromium
 # Test server loads assets from static folder, so make sure files there are up-to-date
 rm -rf static
 npm run build
-python manage.py compilescss
-python manage.py collectstatic --ignore=*.scss
-python manage.py compilescss --delete-files
+python manage.py collectstatic
 
 # Run E2E tests
 python manage.py test bookmarks.e2e --pattern="e2e_test_*.py"
