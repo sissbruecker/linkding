406c38e3f2540383ce6e31cd97d223a597753af8
Sascha Ißbrücker <sascha.issbruecker@gmail.com>
2024-09-15 22:55:56 +0200

    remove asset refresh for now

--- bookmarks/urls.py
+++ bookmarks/urls.py
@@ -36,11 +36,6 @@ urlpatterns = [
         views.bookmarks.details_modal,
         name="details_modal",
     ),
-    path(
-        "bookmarks/<int:bookmark_id>/details_assets",
-        views.bookmarks.details_assets,
-        name="details_assets",
-    ),
     # Assets
     path(
         "assets/<int:asset_id>",
