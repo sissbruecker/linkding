2cfc960d6e279fc5f01a6eec65c741d88b31d120
Justin Mason <github@jmason.org>
2024-12-17 23:58:40 +0000

    Build a local docker image for personal use

--- scripts/build-docker.sh
+++ scripts/build-docker.sh
@@ -1,30 +1,30 @@
 #!/usr/bin/env bash
 
-version=$(<version.txt)
+version=$(cat version.txt)
 
 # Base image
-docker buildx build --target linkding --platform linux/amd64,linux/arm64,linux/arm/v7 \
+docker buildx build --target linkding --platform linux/aarch64 \
   -f docker/default.Dockerfile \
-  -t sissbruecker/linkding:latest \
-  -t sissbruecker/linkding:$version \
-  --push .
+  -t jmason/linkding:latest \
+  -t jmason/linkding:$version \
+  .
 
-docker buildx build --target linkding --platform linux/amd64,linux/arm64,linux/arm/v7 \
-  -f docker/alpine.Dockerfile \
-  -t sissbruecker/linkding:latest-alpine \
-  -t sissbruecker/linkding:$version-alpine \
-  --push .
+# docker buildx build --target linkding --platform linux/amd64,linux/arm64,linux/arm/v7 \
+  # -f docker/alpine.Dockerfile \
+  # -t jmason/linkding:latest-alpine \
+  # -t jmason/linkding:$version-alpine \
+  # --push .
 
 # Plus image with support for single-file snapshots
 # Needs checking if this works with ARMv7, excluded for now
-docker buildx build --target linkding-plus --platform linux/amd64,linux/arm64 \
-  -f docker/default.Dockerfile \
-  -t sissbruecker/linkding:latest-plus \
-  -t sissbruecker/linkding:$version-plus \
-  --push .
-
-docker buildx build --target linkding-plus --platform linux/amd64,linux/arm64 \
-  -f docker/alpine.Dockerfile \
-  -t sissbruecker/linkding:latest-plus-alpine \
-  -t sissbruecker/linkding:$version-plus-alpine \
-  --push .
+# docker buildx build --target linkding-plus --platform linux/amd64,linux/arm64 \
+  # -f docker/default.Dockerfile \
+  # -t jmason/linkding:latest-plus \
+  # -t jmason/linkding:$version-plus \
+  # --push .
+# 
+# docker buildx build --target linkding-plus --platform linux/amd64,linux/arm64 \
+  # -f docker/alpine.Dockerfile \
+  # -t jmason/linkding:latest-plus-alpine \
+  # -t jmason/linkding:$version-plus-alpine \
+  # --push .
