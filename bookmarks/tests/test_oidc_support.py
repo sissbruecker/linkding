85737043256bfd2075c723e960bf283328a87ab6
Sascha Ißbrücker <sascha.issbruecker@gmail.com>
2025-01-30 03:17:19 +0100

    improve tests

--- bookmarks/tests/test_oidc_support.py
+++ bookmarks/tests/test_oidc_support.py
@@ -4,6 +4,8 @@ import os
 from django.test import TestCase, override_settings
 from django.urls import URLResolver
 
+from bookmarks import utils
+
 
 class OidcSupportTest(TestCase):
     def test_should_not_add_oidc_urls_by_default(self):
@@ -55,80 +57,68 @@ class OidcSupportTest(TestCase):
         base_settings = importlib.import_module("siteroot.settings.base")
         importlib.reload(base_settings)
 
-        self.assertEqual(
-            True,
-            base_settings.OIDC_VERIFY_SSL,
-        )
+        self.assertEqual(True, base_settings.OIDC_VERIFY_SSL)
+        self.assertEqual("openid email profile", base_settings.OIDC_RP_SCOPES)
+        self.assertEqual("email", base_settings.OIDC_USERNAME_CLAIM)
 
-        del os.environ["LD_ENABLE_OIDC"]
-
-    def test_username_claim_email(self):
-        os.environ["LD_ENABLE_OIDC"] = "True"
-        base_settings = importlib.import_module("siteroot.settings.base")
-        importlib.reload(base_settings)
-
-        self.assertEqual(
-            "email",
-            base_settings.OIDC_USERNAME_CLAIM,
-        )
-        del os.environ["LD_ENABLE_OIDC"]
+        del os.environ["LD_ENABLE_OIDC"]  # Remove the temporary environment variable
 
     @override_settings(LD_ENABLE_OIDC=True, OIDC_USERNAME_CLAIM="email")
-    def test_username_should_be_email(self):
-        email = "test@example.com"
+    def test_username_should_use_email_by_default(self):
         claims = {
             "email": "test@example.com",
-            "name": "test",
-            "given_name": "test",
-            "preferred_username": "test",
-            "nickname": "test",
+            "name": "test name",
+            "given_name": "test given name",
+            "preferred_username": "test preferred username",
+            "nickname": "test nickname",
             "groups": [],
         }
-        from bookmarks import utils
 
-        username = utils.generate_username(email, claims)
+        username = utils.generate_username(claims["email"], claims)
 
-        self.assertEqual(
-            email,
-            username,
-        )
+        self.assertEqual(claims["email"], username)
 
     @override_settings(LD_ENABLE_OIDC=True, OIDC_USERNAME_CLAIM="preferred_username")
-    def test_username_should_be_preferred_username(self):
-        email = "test@example.com"
+    def test_username_should_use_custom_claim(self):
         claims = {
             "email": "test@example.com",
-            "name": "test",
-            "given_name": "test",
-            "preferred_username": "test",
-            "nickname": "test",
+            "name": "test name",
+            "given_name": "test given name",
+            "preferred_username": "test preferred username",
+            "nickname": "test nickname",
             "groups": [],
         }
-        from bookmarks import utils
 
-        username = utils.generate_username(email, claims)
+        username = utils.generate_username(claims["email"], claims)
 
-        self.assertEqual(
-            "test",
-            username,
-        )
+        self.assertEqual(claims["preferred_username"], username)
 
     @override_settings(LD_ENABLE_OIDC=True, OIDC_USERNAME_CLAIM="nonexistant_claim")
-    def test_username_fallback_to_email(self):
-        email = "test@example.com"
+    def test_username_should_fallback_to_email(self):
         claims = {
             "email": "test@example.com",
-            "name": "test",
-            "given_name": "test",
-            "preferred_username": "test",
-            "nickname": "test",
+            "name": "test name",
+            "given_name": "test given name",
+            "preferred_username": "test preferred username",
+            "nickname": "test nickname",
             "groups": [],
         }
-        from bookmarks import utils
 
-        username = utils.generate_username(email, claims)
+        username = utils.generate_username(claims["email"], claims)
 
-        self.assertEqual(
-            email,
-            username,
-        )
+        self.assertEqual(claims["email"], username)
+
+    @override_settings(LD_ENABLE_OIDC=True, OIDC_USERNAME_CLAIM="preferred_username")
+    def test_username_should_be_normalized(self):
+        claims = {
+            "email": "test@example.com",
+            "name": "test name",
+            "given_name": "test given name",
+            "preferred_username": "ＮｏｒｍａｌｉｚｅｄＵｓｅｒ",
+            "nickname": "test nickname",
+            "groups": [],
+        }
+
+        username = utils.generate_username(claims["email"], claims)
+
+        self.assertEqual("NormalizedUser", username)
