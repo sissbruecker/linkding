22768324650831cc414ff09d30d2abffb75fb30b
Sascha Ißbrücker <sascha.issbruecker@googlemail.com>
2025-02-19 06:44:21 +0100

    Return web archive fallback URL from REST API (#993)

--- bookmarks/tests/test_bookmarks_api.py
+++ bookmarks/tests/test_bookmarks_api.py
@@ -1,15 +1,18 @@
+import datetime
 import urllib.parse
 from collections import OrderedDict
 from unittest.mock import patch
 
 from django.contrib.auth.models import User
 from django.urls import reverse
+from django.utils import timezone
 from rest_framework import status
 from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
 
 from bookmarks.models import Bookmark, BookmarkSearch, UserProfile
 from bookmarks.services import website_loader
+from bookmarks.services.wayback import generate_fallback_webarchive_url
 from bookmarks.services.website_loader import WebsiteMetadata
 from bookmarks.tests.helpers import LinkdingApiTestCase, BookmarkFactoryMixin
 
@@ -33,7 +36,10 @@ class BookmarksApiTestCase(LinkdingApiTestCase, BookmarkFactoryMixin):
             expectation["title"] = bookmark.title
             expectation["description"] = bookmark.description
             expectation["notes"] = bookmark.notes
-            expectation["web_archive_snapshot_url"] = bookmark.web_archive_snapshot_url
+            expectation["web_archive_snapshot_url"] = (
+                bookmark.web_archive_snapshot_url
+                or generate_fallback_webarchive_url(bookmark.url, bookmark.date_added)
+            )
             expectation["favicon_url"] = (
                 f"http://testserver/static/{bookmark.favicon_file}"
                 if bookmark.favicon_file
@@ -589,6 +595,23 @@ class BookmarksApiTestCase(LinkdingApiTestCase, BookmarkFactoryMixin):
         url = reverse("bookmarks:bookmark-detail", args=[bookmark.id])
         response = self.get(url, expected_status_code=status.HTTP_200_OK)
         self.assertBookmarkListEqual([response.data], [bookmark])
+
+    def test_get_bookmark_returns_fallback_webarchive_url(self):
+        self.authenticate()
+        bookmark = self.setup_bookmark(
+            web_archive_snapshot_url="",
+            url="https://example.com/",
+            added=timezone.datetime(
+                2023, 8, 11, 21, 45, 11, tzinfo=datetime.timezone.utc
+            ),
+        )
+
+        url = reverse("bookmarks:bookmark-detail", args=[bookmark.id])
+        response = self.get(url, expected_status_code=status.HTTP_200_OK)
+        self.assertEqual(
+            response.data["web_archive_snapshot_url"],
+            "https://web.archive.org/web/20230811214511/https://example.com/",
+        )
 
     def test_update_bookmark(self):
         self.authenticate()
