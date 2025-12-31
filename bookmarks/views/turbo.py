ef7685ae674becc642b6495fbc3f9fa6fc9467ba
Sascha Ißbrücker <sascha.issbruecker@gmail.com>
2025-12-31 18:35:21 +0100

    Move tag management forms into dialogs

--- bookmarks/views/turbo.py
+++ bookmarks/views/turbo.py
@@ -1,5 +1,6 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render as django_render
+from django.template import loader
 
 
 def accept(request: HttpRequest):
@@ -15,5 +16,22 @@ def is_frame(request: HttpRequest, frame: str) -> bool:
 
 def stream(request: HttpRequest, template_name: str, context: dict) -> HttpResponse:
     response = django_render(request, template_name, context)
+    response["Content-Type"] = "text/vnd.turbo-stream.html"
+    return response
+
+
+def replace(
+    request: HttpRequest, target_id: str, template_name: str, context: dict, status=None
+) -> HttpResponse:
+    """
+    Returns a Turbo steam for replacing a specific target with the rendered
+    template. Mostly useful for updating forms in place after failed submissions,
+    without having to create a separate template.
+    """
+    if status is None:
+        status = 200
+    content = loader.render_to_string(template_name, context, request)
+    stream_content = f'<turbo-stream action="replace" method="morph" target="{target_id}"><template>{content}</template></turbo-stream>'
+    response = HttpResponse(stream_content, status=status)
     response["Content-Type"] = "text/vnd.turbo-stream.html"
     return response
