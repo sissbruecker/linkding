184e4baa842dd9301eaa1a50db8286fc4f40ec7a
Sascha Ißbrücker <sascha.issbruecker@googlemail.com>
2026-01-05 18:41:50 +0100

    Add option to run supervisor as main process (#1270)
    * Add option to run supervisor as main process
    
    * use new option in test script

--- scripts/run-docker.sh
+++ scripts/run-docker.sh
@@ -9,4 +9,5 @@ docker rm -f linkding-local || true
 docker run --name linkding-local --rm -p 9090:9090  \
   -e LD_SUPERUSER_NAME=admin \
   -e LD_SUPERUSER_PASSWORD=admin \
+  -e LD_SUPERVISOR_MANAGED=True \
   sissbruecker/linkding:local
